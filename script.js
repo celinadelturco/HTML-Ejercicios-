4b0214e484a91989f61f46b803c68cab17f6c875
Celina <celidelturco2@gmail.com>
2024-01-23 18:32:35 -0300

    intento de fondo y login

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ function abrirGaleria(imagen) {
 function cerrarGaleria() {
     document.getElementById("galeriaEmergente").style.display = "none";
 }
+function login(){
+            var username = document.getElementById("username").value;
+            var password = document.getElementById("password").value;
+
+            if (username ==="celina" && password === "1234"){
+                window.location.href = "index.html";
+            } else {
+                alert("Usuario o contraseña incorrectos");
+            }
+        }
+
